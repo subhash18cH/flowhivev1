f02cfcd7a22f5dbfb30e8a8da336e86d2264faa4
subhash18cH <subhashcnd15@gmail.com>
2025-05-02 22:46:24 +0530

    added userModel and db connection

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+const dotenv=require("dotenv").config();
+
+const dbConnection = require("./db/db");
+const cors=require("cors")
+dbConnection();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
+app.get("/",(req,res)=>{
+  res.send("Welcome to floedev app")
+})
+
+app.listen(process.env.PORT,()=>{
+  console.log("server is running");
+})
