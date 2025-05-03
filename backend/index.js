e9074365571e4c75c44ff0cb22be375019192696
subhash18cH <subhashcnd15@gmail.com>
2025-05-03 17:50:53 +0530

    removed bugs

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://flowhive.netlify.app/',
   credentials: true,
 }));
 
